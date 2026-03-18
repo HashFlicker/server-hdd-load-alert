821d17051e15ac6d727786c23d5458929b48fef4
brata <brata@bumantara.co.id>
2026-03-18 15:08:58 +0700

    merge disk usage, size & percent query to 1 function

--- lib/eventHandler.py
+++ lib/eventHandler.py
@@ -1,13 +1,12 @@
-import requests
-from dotenv import load_dotenv
+import requests,os
 from logs import event_log
-import os, json
+from dotenv import load_dotenv
 
 load_dotenv()
 PROMETHEUS_API = os.getenv("PROMETHEUS_API")
 
 
-def prometheus_query(query):
+def prometheus_query(query:str):
     url = f"{PROMETHEUS_API}/api/v1/query"
     event_log.info(f"Fetching Prometheus API data")
     try:
@@ -17,60 +16,55 @@ def prometheus_query(query):
     except Exception as e:
         print(f"Error fetching data: {e}")
         return []
-    
 
-def queryDiskInGB(serverIP:str):
+def disk(serverIP:str):
     
     metric = {}
             
-    query = (
+    diskUsageQuery = (
         f'(windows_logical_disk_size_bytes{{job="{serverIP}", volume!~"Harddisk.*"}} '
-        f'- windows_logical_disk_free_bytes{{job="{serverIP}", volume!~"Harddisk.*"}}) '
-        f'/ 1073741824'
+        f'- windows_logical_disk_free_bytes{{job="{serverIP}", volume!~"Harddisk.*"}}) / 1073741824'
     )
-
-    queryResult = prometheus_query(query) 
-    for node in queryResult:
-        instance = node['metric'].get('job', 'Unknown')
-        disk = node['metric'].get('volume')
-        usage = float(node['value'][1])
-        
-        metric['instance'] = instance
-        if disk == "D:":
-            metric['Disk D'] = f'{usage:.2f} GB'
-        else:
-            metric['Dick C'] = f'{usage:.2f} GB'
     
-    return print(metric)
-
-
-def queryDiskInPercent(serverIP:str):
+    diskTotalQuery = (
+        f'windows_logical_disk_size_bytes{{job="{serverIP}", volume!~"Harddisk.*"}} / 1073741824'
+    )
     
-    metric = {}
-
-    query = (
+    diskPercentquery = (
         f'(windows_logical_disk_size_bytes{{job="{serverIP}", volume!~"Harddisk.*"}} '
         f'- windows_logical_disk_free_bytes{{job="{serverIP}", volume!~"Harddisk.*"}}) '
         f'/ windows_logical_disk_size_bytes{{job="{serverIP}", volume!~"Harddisk.*"}} * 100'
     )
 
-    result = prometheus_query(query)
-    for node in result:
-        instance = node['metric'].get('job', 'Unknown')
-        disk = node['metric'].get('volume')
-        usage = float(node['value'][1])
-        
-        metric['instance'] = instance
-        if disk == "D:":
-            metric['Disk D'] = f'{usage:.2f} %'
-        else:
-            metric['Dick C'] = f'{usage:.2f} %'
+    diskTotal = prometheus_query(diskTotalQuery)
+    diskUsage = prometheus_query(diskUsageQuery) 
+    diskPercent = prometheus_query(diskPercentquery)
     
+    for node in diskTotal:
+        instance = node['metric'].get('job', 'Unknown')
+        metric['instance'] = instance    
+        
+        for node2 in diskUsage:
+            diskUsagePartition = node2['metric'].get('volume')
+            usage = float(node2['value'][1])
+            if diskUsagePartition == 'D:':
+                metric['Disk D usage'] = f'{usage:.2f} GB'
+            else:
+                metric['Disk C usage'] = f'{usage:.2f} GB' 
+                
+        for node3 in diskPercent:
+            diskUsagePartition = node3['metric'].get('volume')
+            Percent = float(node3['value'][1])
+            if diskUsagePartition == 'D:':
+                metric['Disk D usage percent'] = f'{Percent:.2f} %'
+            else:
+                metric['Disk C usage percent'] = f'{Percent:.2f} %' 
+        
+        disk = node['metric'].get('volume')
+        size = float(node['value'][1])
+        if disk == "D:":
+            metric['Disk D size'] = f'{size:.2f} GB'
+        else:
+            metric['Dick C size'] = f'{size:.2f} GB'
+        
     return print(metric)
-
-
-
-# For Test Purposes
-# if __name__ == "__main__":
-#     queryDiskInPercent("192.168.22.9")
-#     queryDiskInGB("192.168.22.9")
